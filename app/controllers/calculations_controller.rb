8ae42d372a1c9953c742741c52d21abeebd3807f
cflicher <cflicher@users.noreply.github.com>
2016-11-06 15:18:05 -0600

    final updates

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -13,4 +13,26 @@ class CalculationsController < ApplicationController
     @square_number = (@number**0.5).round(2)
   end
 
+  def random
+    @min = params[:MIN].to_i
+    @max = params[:MAX].to_i
+  end
+
+  def payment
+    @annual_interest_rate = (params[:INTEREST_RATE]).to_f/100 #annual interest rate
+    @monthly_interest_rate = @annual_interest_rate/12
+    @monthpct = @monthly_interest_rate/100
+    @years = params[:NUMBER_OF_YEARS].to_i
+    @months = @years*12
+    @principal = params[:PRINCIPAL_VALUE].to_i
+    @numerator = @monthpct*@principal
+    @oneplusr = 1+@monthpct
+    @negperiod = (-1)*@months
+    @oneplusrtomonth = @oneplusr**@negperiod
+    @denominator = 1-@oneplusrtomonth
+    @payment = (@numerator/@denominator).round(2)
+
+  end
+
+
 end
