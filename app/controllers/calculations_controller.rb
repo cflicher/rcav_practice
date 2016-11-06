e3e37be767500451943d9d3f9f84e68b5b5423db
cflicher <cflicher@users.noreply.github.com>
2016-11-06 14:33:00 -0600

    work in progress

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,15 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @number = params[:NUMBER].to_i
+    @square_number = @number**2
+  end
+
+  def square_root
+    @number = params[:NUMBER].to_i
+    @square_number = (@number**0.5).round(2)
+  end
+
 end
